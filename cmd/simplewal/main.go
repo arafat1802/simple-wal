269425188d0c8c2ae88f7a54873856223df9ea95
arafat1802 <arafat.hossain1802@gmail.com>
2025-05-31 16:16:25 +0600

    try to setup bazzle and gazzle

--- cmd/simplewal/main.go
+++ cmd/simplewal/main.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 
 	"simple-wal/internal/wal"
-	"google.golang.org/protobuf/proto"
+	// "google.golang.org/protobuf/proto"
 )
 
 func usage() {
--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module simple-wal
 
 go 1.24.3
+
